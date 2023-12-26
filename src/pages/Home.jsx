3fb60cc91e2de3870370b78e1566bcb36bfe9957
Ashutosh Sabale <ashutoshsabale191@gmail.com>
2023-12-26 18:35:41 +0530

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home(){
                       <div className="flex flex-wrap h-[43vh] justify-center items-center">
                           <div className="p-2 w-full">
                               <h1 className="text-2xl font-bold hover:text-gray-500">
-                                  LogIn to add Post
+                                  LogIn to add and Read Post
                               </h1>
                           </div>
                       </div>
